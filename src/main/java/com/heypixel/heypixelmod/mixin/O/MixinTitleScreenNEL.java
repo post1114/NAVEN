7075a04ae46adff6fa2f30b345c0725b0de0cdc9
post1114 <post1114@users.noreply.github.com>
2026-08-20 22:20:57 +0800

    Fix crash: use Shadow to access protected addRenderableWidget in MixinTitleScreenNEL; disable gradle wrapper URL validation

--- src/main/java/com/heypixel/heypixelmod/mixin/O/MixinTitleScreenNEL.java
+++ src/main/java/com/heypixel/heypixelmod/mixin/O/MixinTitleScreenNEL.java
@@ -1,60 +1,38 @@
 package com.heypixel.heypixelmod.mixin.O;
 
 import com.heypixel.heypixelmod.obsoverlay.nel.NelManager;
-import java.lang.reflect.Field;
-import java.util.List;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin({TitleScreen.class})
-public class MixinTitleScreenNEL {
+public abstract class MixinTitleScreenNEL extends Screen {
 
-   @Unique
-   private static Field naven_renderables;
-   @Unique
-   private static Field naven_children;
-   @Unique
-   private static Field naven_narratables;
+    protected MixinTitleScreenNEL() {
+        super(null);
+    }
 
-   static {
-      try {
-         naven_renderables = Screen.class.getDeclaredField("renderables");
-         naven_renderables.setAccessible(true);
-         naven_children = Screen.class.getDeclaredField("children");
-         naven_children.setAccessible(true);
-         naven_narratables = Screen.class.getDeclaredField("narratables");
-         naven_narratables.setAccessible(true);
-      } catch (NoSuchFieldException e) {
-         e.printStackTrace();
-      }
-   }
+    @Shadow
+    protected abstract Button addRenderableWidget(Button $$0);
 
-   @Inject(
-      method = {"init"},
-      at = {@At("TAIL")}
-   )
-   private void onInit(CallbackInfo ci) {
-      Screen screen = (Screen) (Object) this;
-      int centerX = screen.width / 2;
-      int rightX = centerX + 105;
+    @Inject(
+       method = {"init"},
+       at = {@At("TAIL")}
+    )
+    private void onInit(CallbackInfo ci) {
+       int centerX = this.width / 2;
+       int rightX = centerX + 105;
 
-      Button nelButton = Button.builder(
-         net.minecraft.network.chat.Component.literal("NEL"),
-         button -> NelManager.getInstance().openNelScreen()
-      ).bounds(rightX, screen.height / 4 + 48 + 72, 98, 20).build();
+       Button nelButton = Button.builder(
+          net.minecraft.network.chat.Component.literal("NEL"),
+          button -> NelManager.getInstance().openNelScreen()
+       ).bounds(rightX, this.height / 4 + 48 + 72, 98, 20).build();
 
-      try {
-         ((List) naven_renderables.get(screen)).add(nelButton);
-         ((List) naven_children.get(screen)).add(nelButton);
-         ((List) naven_narratables.get(screen)).add(nelButton);
-      } catch (IllegalAccessException e) {
-         e.printStackTrace();
-      }
-   }
+       this.addRenderableWidget(nelButton);
+    }
 }
